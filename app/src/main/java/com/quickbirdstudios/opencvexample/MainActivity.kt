c3a265d28a789b863fe304f0108efa4d78eafda2
xin.zhou <14442626@qq.com>
2025-03-01 18:54:29 +0800

    app可以在模拟器运行，但显示Unable to load OpenCV

--- app/src/main/java/com/quickbirdstudios/opencvexample/MainActivity.kt
+++ app/src/main/java/com/quickbirdstudios/opencvexample/MainActivity.kt
@@ -5,10 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.quickbirdstudios.opencvexample.databinding.ActivityMainBinding
+import org.opencv.android.BaseLoaderCallback
+import org.opencv.android.LoaderCallbackInterface
 import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val TAG = "MainActivity"
+
+    private val mLoaderCallback = object : BaseLoaderCallback(this) {
+        override fun onManagerConnected(status: Int) {
+            when (status) {
+                LoaderCallbackInterface.SUCCESS -> {
+                    Log.i(TAG, "OpenCV loaded successfully")
+                }
+                else -> {
+                    super.onManagerConnected(status)
+                }
+            }
+        }
+    }
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +40,17 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.tvStatus.text = "OpenCV Loaded Successfully!"
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization")
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback)
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!")
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS)
+        }
     }
 
     companion object {
